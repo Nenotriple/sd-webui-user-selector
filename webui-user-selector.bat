ad445e9099d913d1812de4cd647c5cb2be45f97d
Nenotriple <70049990+Nenotriple@users.noreply.github.com>
2023-09-26 03:59:02 -0700

    Update webui-user-selector.bat

--- webui-user-selector.bat
+++ webui-user-selector.bat
@@ -15,17 +15,17 @@ choice /C 1234 /N /M "Choice (1-4): "
 
 if errorlevel 4 (
     echo Updating...
-    set COMMANDLINE_ARGS= --reinstall-xformers --reinstall-torch --xformers --autolaunch
+    set COMMANDLINE_ARGS= --reinstall-xformers --reinstall-torch --xformers
     call git pull
 ) else if errorlevel 3 (
     echo Starting...
-    set COMMANDLINE_ARGS= --xformers --autolaunch --lowvram
+    set COMMANDLINE_ARGS= --xformers --lowvram
 ) else if errorlevel 2 (
     echo Starting...
-    set COMMANDLINE_ARGS= --xformers --autolaunch --medvram
+    set COMMANDLINE_ARGS= --xformers --medvram
 ) else if errorlevel 1 (
     echo Starting...
-    set COMMANDLINE_ARGS= --xformers --autolaunch
+    set COMMANDLINE_ARGS= --xformers
 )
 
 call webui.bat
